5d0631fd3fec8558bfde4031d61e0b7795bf0d4e
Thisseas X <thisseasx@yahoo.com>
2020-02-17 20:33:44 +0200

    Updates

---
+++ precache-manifest.1c4ded3855cb17d4e03324785da375ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb5f7c02a8daa60734ae59342d237e4",
+    "url": "/basic-react/index.html"
+  },
+  {
+    "revision": "79c255af01178194ba74",
+    "url": "/basic-react/static/css/main.69450e88.chunk.css"
+  },
+  {
+    "revision": "c9522e5c87f965d9154e",
+    "url": "/basic-react/static/js/2.a1630c05.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/basic-react/static/js/2.a1630c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c255af01178194ba74",
+    "url": "/basic-react/static/js/main.5bfdbe92.chunk.js"
+  },
+  {
+    "revision": "dfd73c629967baa9c064",
+    "url": "/basic-react/static/js/runtime-main.41fec2f7.js"
+  }
+]);
